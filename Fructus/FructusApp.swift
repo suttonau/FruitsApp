cee8ac7f822af0e5d94440c7023ff9b2aff36487
Sean Sutton <sutton_mjbc@icloud.com>
2021-01-22 13:07:07 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Sean Sutton on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
