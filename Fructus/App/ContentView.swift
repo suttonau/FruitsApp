96e2538cb27c1f8de959a80cfe4925a1d0837aef
Sean Sutton <sutton_mjbc@icloud.com>
2021-02-22 08:31:14 -0500

    "adding fruit card view styling and start button"

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -9,10 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, Sean Sutton!")
             .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
